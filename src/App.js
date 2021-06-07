b7dbfa666a338f5f327a177db7f24fdbf8ce1c8b
mbish08 <mdrew8@gmail.com>
2021-06-07 13:25:17 -0700

    testing fetch to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/plant_groups')
+      .then(res => res.json())
+      .then(groups => console.log(groups))
+
+      fetch('http://localhost:3000/api/v1/plant_types')
+      .then(res => res.json())
+      .then(types => console.log(types))
+  }
+
   render() {
     return (
       <div>
-        
+        App
       </div>
     );
   }
