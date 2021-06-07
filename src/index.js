a4d2d1ae39791ca5122b19d40a72ac741feb04bb
mbish08 <mdrew8@gmail.com>
2021-06-07 14:35:55 -0700

    installed packages and initial redux/store setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux' 
+
+
 
 ReactDOM.render(
   <React.StrictMode>
