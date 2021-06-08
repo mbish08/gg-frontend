8a160d8f102a24a987eb059c197d5e4669b77fa6
mbish08 <mdrew8@gmail.com>
2021-06-08 16:20:13 -0700

    testing PlantGroupList added key

--- src/components/PlantGroupList.js
+++ src/components/PlantGroupList.js
@@ -5,11 +5,9 @@ const PlantGroupList = (props) => {
     // look at destructuring argument vs props
     return (
         <div>
-            {props.plantGroups.map(plantGroup => <li>{plantGroup.name}</li> )}
+            {props.plantGroups.map(plantGroup => <li key={plantGroup.id}>{plantGroup.name}</li> )}
         </div>
     );
 }
-
-
 
 export default PlantGroupList;
