37c46e288219aefccb91c54bace4af886d6a9546
mbish08 <mdrew8@gmail.com>
2021-06-08 11:48:23 -0700

    import reducer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux' 
+import plantGroupReducer from './reducers/plantGroupreducer'
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(plantGroupReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
