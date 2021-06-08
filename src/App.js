7bef841f9d55731be6483d0dab52777eba082153
mbish08 <mdrew8@gmail.com>
2021-06-08 12:58:18 -0700

    add connect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class App extends Component {
 
@@ -22,4 +23,10 @@ class App extends Component {
   }
 }
 
-export default App;
+// const mapStateToProps = (state) => {
+//   return {
+//     plantGroups: state.plantGroups
+//   }
+// }
+
+export default connect(null, {fetchPlantGroups})(App);
