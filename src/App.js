4b1d997cae594aa1a096e8dedc803ce1b240e9d0
mbish08 <mdrew8@gmail.com>
2021-06-08 19:51:22 -0700

    cleanup - removed unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import PlantGroupContainer from './containers/PlantGroupContainer'
 
-class App extends Component {
 
-  // initial fetch testing
-  // componentDidMount() {
-  //   this.props.fetchPlantGroups()
+class App extends Component {
 
   //     fetch('http://localhost:3000/api/v1/plant_types')
   //     .then(res => res.json())
   //     .then(types => console.log(types))
-  // }
 
   render() {
     return (
