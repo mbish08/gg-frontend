e279bb065987f6f5b8583102e04beeafb14e71d4
mbish08 <mdrew8@gmail.com>
2021-06-09 11:42:47 -0700

    add PlantGroupShow component

--- src/components/PlantGroupList.js
+++ src/components/PlantGroupList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PlantGroupShow from './PlantGroupShow';
 
 const PlantGroupList = (props) => {
 
     // look at destructuring argument vs props
     return (
         <div>
+            <PlantGroupShow />
             {props.plantGroups.map(plantGroup => <li key={plantGroup.id}>{plantGroup.name}</li> )}
         </div>
     );
---
+++ src/components/PlantGroupShow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlantGroupShow = (props) => {
+    return (
+        <div>
+            Plant Group Show test
+        </div>
+    );
+}
+
+export default PlantGroupShow;
