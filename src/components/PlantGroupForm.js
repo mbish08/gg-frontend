99947af8171132123b067e47ee946ea408066fff
mbish08 <mdrew8@gmail.com>
2021-06-08 20:29:23 -0700

    added handleChange, initial handleSubmit

--- src/components/PlantGroupForm.js
+++ src/components/PlantGroupForm.js
@@ -7,14 +7,21 @@ class PlantGroupForm extends Component {
     }
 
     handleChange = (event) => {
-        
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = () => {
+
     }
 
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" value={this.state.name} onChange={this.handleChange} placeholder="New Plant Group Name" /><br/>
+                <h4>Add a new Plant Group</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.name} name="name" onChange={this.handleChange} placeholder="New Plant Group Name" /><br/>
                     <input type="submit" />
                 </form>
             </div>
