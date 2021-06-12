eaad92aa18925b84e29accb677313e4b51eb9cbd
mbish08 <mdrew8@gmail.com>
2021-06-12 15:30:39 -0700

    adjusted spacing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,9 @@ const link = {
 const Navigation = () => {
     return (
         <div>
-            <NavLink to='/' exact style={link} activeStyle={{background: 'darkgreen'}}>Home</NavLink>
+            <NavLink to='/' exact style={link} activeStyle={{background: 'darkgreen'}}>Home</NavLink> {""}
+            <NavLink to='/plant_groups' exact style={link} activeStyle={{background: 'darkgreen'}}>Current Plant Groups</NavLink> {""}
+            <NavLink to='/plant_groups/new' exact style={link} activeStyle={{background: 'darkgreen'}}>Add a New Plant Group</NavLink>
         </div>
     );
 }
