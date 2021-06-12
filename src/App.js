e604bb76a103c561601fb6939839f00cb2634704
mbish08 <mdrew8@gmail.com>
2021-06-12 14:01:19 -0700

    initial homepage set up in App.js and Home.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PlantGroupContainer from './containers/PlantGroupContainer'
-
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
 
@@ -11,6 +11,9 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
         <PlantGroupContainer />
       </div>
     );
