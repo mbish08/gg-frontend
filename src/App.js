d2f89b485e1d9ea084f9162702daa8486f6652f9
mbish08 <mdrew8@gmail.com>
2021-06-12 14:36:13 -0700

    cleanup unused code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import PlantGroupContainer from './containers/PlantGroupContainer'
 
 class App extends Component {
 
-  //     fetch('http://localhost:3000/api/v1/plant_types')
-  //     .then(res => res.json())
-  //     .then(types => console.log(types))
-
   render() {
     return (
       <div>
