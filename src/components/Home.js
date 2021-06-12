4886eb7a4506fe8436eb13346d186696bfd67724
mbish08 <mdrew8@gmail.com>
2021-06-12 16:17:10 -0700

    add verbiage to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
+           <h1>
             Welcome to Garden Gnome!
+           </h1>
+           <p>
+               This app is designed to help you keep track of all of your plants.<br/><br/>
+               Plants are sorted into groups (such as roses, palm trees, agave, etc).<br/><br/>
+               Add new plants as you get them and all of the info you need so you have everything in one place!<br/><br/>
+               Click on Current Plant Groups at the top to get started.
+           </p>
         </div>
     );
 }
