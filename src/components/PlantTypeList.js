5bb1ff9ae9103d1a4abdafec6ed1253676837fb1
mbish08 <mdrew8@gmail.com>
2021-06-12 17:25:30 -0700

    now displays attributes for plant types

--- src/components/PlantTypeList.js
+++ src/components/PlantTypeList.js
@@ -11,8 +11,16 @@ const PlantTypeList = (props) => {
     return (
         <div>
             {props.plantTypes && props.plantTypes.map(plantType => 
-            <li key={plantType.id}>{plantType.name} 
-                <button onClick={() => handleDelete(plantType)}>Delete</button></li>
+            <span key={plantType.id}>{plantType.name} 
+                <button onClick={() => handleDelete(plantType)}>Delete</button>
+                <li>Fertilizer Type:  {plantType.fert_type}</li>
+                <li>Fertilizer Schedule:  {plantType.fert_sched}</li>
+                <li>Water Needs:  {plantType.water}</li>
+                <li>Soil pH:  {plantType.soil_ph}</li>
+                <li>Soil Type:  {plantType.soil_type}</li>
+                <li>Misc Info:  {plantType.misc_info}</li>
+                <br/><br/>
+            </span>
             )}
         </div>
     );
