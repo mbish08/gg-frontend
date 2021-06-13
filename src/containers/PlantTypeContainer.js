73f9bbb5b898e62584b59af1d845c28e2ec8bd3f
mbish08 <mdrew8@gmail.com>
2021-06-12 17:59:49 -0700

    broke url input

--- src/containers/PlantTypeContainer.js
+++ src/containers/PlantTypeContainer.js
@@ -3,13 +3,13 @@ import PlantTypeForm from '../components/PlantTypeForm';
 import PlantTypeList from '../components/PlantTypeList';
 
 class PlantTypeContainer extends Component {
+ 
     render() {
+
         return (
             <div>
-                <h3>Here are your {this.props.group.name}:</h3>
-                <PlantTypeList plantTypes={this.props.group && this.props.group.plant_types} />
-                <h3>Add a new Type of {this.props.group.name}</h3>
                 <PlantTypeForm plantGroup={this.props.group} />
+                <PlantTypeList plantTypes={this.props.group && this.props.group.plant_types} />
             </div>
         );
     }
