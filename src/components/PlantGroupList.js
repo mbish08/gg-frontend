6e787bfca8247a3de3e0c1a5ad619f40a7d6b708
mbish08 <mdrew8@gmail.com>
2021-07-12 14:39:54 -0700

    sort button started

--- src/components/PlantGroupList.js
+++ src/components/PlantGroupList.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 
-const PlantGroupList = (props) => {
+class PlantGroupList extends Component {
     
-    return (
-        <div>
-            {props.plantGroups.map(plantGroup => 
-                <div key={plantGroup.id}>
-                    <Link to={`/plant_groups/${plantGroup.id}`}>{plantGroup.name}: {plantGroup.plant_types.length} types</Link>
-                </div> )}
-        </div>
-    );
+    handleClick = (event) => {
+        // debugger;
+        parseInt(event.target.value) === 0 ? event.target.value = 1 : event.target.value = 0
+        console.log(event.target.value)
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick} value={0} >Sort</button>
+                
+                {this.props.plantGroups.map(plantGroup => 
+                    <div key={plantGroup.id}>
+                        <Link to={`/plant_groups/${plantGroup.id}`}>{plantGroup.name}: {plantGroup.plant_types.length} types</Link>
+                    </div> )}
+            </div>
+        );
+    }
+    
 }
 
 export default PlantGroupList;
