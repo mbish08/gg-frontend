ac078d72ed321d58ce57ba1f822c7c08502c8a25
mbish08 <mdrew8@gmail.com>
2021-07-12 14:51:19 -0700

    add count to state

--- src/components/PlantGroupList.js
+++ src/components/PlantGroupList.js
@@ -4,22 +4,31 @@ import { Link } from 'react-router-dom'
 
 
 class PlantGroupList extends Component {
+
+    state = {
+        count: 0
+    }
     
     handleClick = (event) => {
         // debugger;
         parseInt(event.target.value) === 0 ? event.target.value = 1 : event.target.value = 0
         console.log(event.target.value)
+        this.setState({
+            count: parseInt(event.target.value)
+        })
+        debugger;
     }
 
     render() {
         return (
             <div>
                 <button onClick={this.handleClick} value={0} >Sort</button>
-                
+            
                 {this.props.plantGroups.map(plantGroup => 
                     <div key={plantGroup.id}>
                         <Link to={`/plant_groups/${plantGroup.id}`}>{plantGroup.name}: {plantGroup.plant_types.length} types</Link>
-                    </div> )}
+                    </div> 
+                )}
             </div>
         );
     }
